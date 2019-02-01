d9d2f17a47aa6781f5b1edb9a2e8dbadf5833b3a
Ben <doc.ben.johnson@gmail.com>
2019-02-01 12:56:42 -0500

    shuffle works!

--- controller.js
+++ controller.js
@@ -4,4 +4,11 @@ $('span').mouseover(function() {
 
 $('#shuffle').click(function(){
     let sampled = [];
+    // adapted from https://jsfiddle.net/C6LPY/2/
+    var parent = $("#quiz");
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+    $('span').css('background-color', 'black')
 });
