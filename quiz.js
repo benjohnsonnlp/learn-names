2781f5e912f3ff4ddca216be902b964a4892ae8b
Ben <doc.ben.johnson@gmail.com>
2020-01-17 16:18:27 -0500

    Refactoring + dealing with missing pictures

--- quiz.js
+++ quiz.js
@@ -9,24 +9,50 @@ theIframe.find(".PSHYPERLINKDISABLED").each(function() {
     names.push($(this).text());
 });
 
-theIframe.find(".PSIMAGE").each(function() {
-    images.push($(this).attr('src'));
+first = true
+theIframe.find(".PSGROUPBOXWBO").each(function() {
+    if (!first) {
+        found = false;
+        $(this).find(".PSIMAGE").each(function () {
+            images.push($(this).attr('src'));
+            found = true;
+        })
+        if (!found) {
+            images.push("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTkysqug-XJdJ3_8oHxn33gCcksEvKuS7-RArWgHwrpKx8SM8EKvw&s")
+        }
+    } else {
+        first = false;
+    }
 });
 
 let zipped = names.map(function(e, i) {
     return [e, images[i]];
 });
 
+let studentInfo = zipped.map(function(item, i) {
+   return {
+       image: item[1],
+       name: item[0],
+       id: i,
+       learned: false,
+   }
+});
+
 let id = 0;
-$.each(zipped, function(){
+$.each(studentInfo, function(){
     quizHtml +=
         '<div style="height:120px;width:300px;display:inline-block">' +
-        '<img studentId=\' + id + \' src="' + this[1] + '" style="width:100px; height:100px"/>'+
-        '<span style="background-color: black" studentId=' + id + '>' + this[0] + '</span>' +
-        '</div>';
+        '<img studentId="' + this.id + '" src="' + this.image + '" style="width:100px; height:100px"/>'+
+        '<span style="background-color: black" studentId=' + this.id + '>' + this.name +
+        '<form><input type="checkbox" value="true"/>Learned</form></span></div>';
     id++;
-
 });
+
+quizHtml += "</div>";
+
+quizHtml += "<div id='learned'>";
+
+
 
 quizHtml += "</div>";
 
